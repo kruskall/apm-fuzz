3390b5822501b0bb82bd515bfdf04ebf7d51b306
kruskall <99559985+kruskall@users.noreply.github.com>
2023-07-20 17:46:40 +0200

    feat: update fuzzer code to go-fuzz-headers

--- fuzz/fuzz.go
+++ fuzz/fuzz.go
@@ -3,10 +3,11 @@ package fuzz
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
-	"github.com/google/gofuzz"
 	rumv3 "github.com/kruskall/apm-fuzz/internal/rumv3"
 	v2 "github.com/kruskall/apm-fuzz/internal/v2"
+	fuzz "github.com/kruskall/go-fuzz-headers"
 )
 
 func GenerateIntakeV2Data(data []byte, opts ...Option) ([]byte, error) {
@@ -42,57 +43,73 @@ func GenerateRumv3Data(data []byte, opts ...Option) ([]byte, error) {
 	return out, nil
 }
 
-func newFuzzer(data []byte, opts ...Option) *fuzz.Fuzzer {
+func newFuzzer(data []byte, opts ...Option) *fuzz.ConsumeFuzzer {
 	conf := defaultConfig()
 	for _, opt := range opts {
 		opt(conf)
 	}
 
-	fuzzer := fuzz.NewFromGoFuzz(data).
-		NilChance(conf.nilChance).
-		AllowUnexportedFields(conf.allowUnexportedFields).
-		MaxDepth(conf.maxDepth).
-		Funcs(func(a *any, c fuzz.Continue) {
-			switch c.Intn(6) {
-			case 0:
-				*a = c.RandString()
-			case 1:
-				*a = c.Float64()
-			case 2:
-				*a = []any{c.Float64(), c.RandString(), nil}
-			case 3:
-				*a = map[string]any{
-					c.RandString(): c.Float64(),
-					c.RandString(): c.RandString(),
-					c.RandString(): c.Int(),
-					c.RandString(): nil,
-					c.RandString(): []string{c.RandString()},
-				}
-			case 4:
-				*a = []any{
-					c.Float64(),
-					nil,
-					[]any{c.Float64(), c.RandString(), nil},
-					map[string]any{
-						c.RandString(): []string{c.RandString()},
-						c.RandString(): c.RandString(),
-						c.RandString(): c.Int(),
-						c.RandString(): nil,
-					},
-				}
-			case 5:
-				*a = nil
-			}
-		})
+	fuzzer := fuzz.NewConsumer(data,
+		fuzz.WithNilChance(float32(conf.nilChance)),
+		fuzz.WithMaxDepth(int64(conf.maxDepth)),
+		fuzz.WithUnknownTypeStrategy(fuzz.FailWithError),
+		fuzz.WithCustomFunction(fuzzAny),
+		fuzz.WithCustomFunction(fuzzTime),
+	)
 
 	return fuzzer
 }
 
-func generateData(fuzzer *fuzz.Fuzzer, models ...any) ([]byte, error) {
+func fuzzTime(a *time.Time, c fuzz.Continue) error {
+	sec, err := c.Source.GetInt()
+	if err != nil {
+		return err
+	}
+
+	nsec, err := c.Source.GetInt()
+	if err != nil {
+		return err
+	}
+
+	*a = time.Unix(int64(sec), int64(nsec))
+	return nil
+}
+
+func fuzzAny(a *any, c fuzz.Continue) error {
+	rnd, err := c.Source.GetByte()
+	if err != nil {
+		return err
+	}
+
+	switch rnd % 5 {
+	case 0:
+		*a, err = c.Source.GetString()
+	case 1:
+		*a, err = c.Source.GetFloat64()
+	case 2:
+		s := make([]any, 0)
+		if err = c.GenerateStruct(&s); err == nil {
+			*a = s
+		}
+	case 3:
+		m := make(map[string]any)
+		if err = c.GenerateStruct(&m); err == nil {
+			*a = m
+		}
+	case 4:
+		*a = nil
+	}
+
+	return err
+}
+
+func generateData(fuzzer *fuzz.ConsumeFuzzer, models ...any) ([]byte, error) {
 	var out []byte
 
 	for _, model := range models {
-		fuzzer.Fuzz(model)
+		if err := fuzzer.GenerateStruct(model); err != nil {
+			return nil, fmt.Errorf("failed to generate model %T: %w", model, err)
+		}
 
 		b, err := json.Marshal(model)
 		if err != nil {
